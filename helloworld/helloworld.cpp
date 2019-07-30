c46cf4c675b66acc90db48af2505320de3f1aab9
Your Name <you@example.com>
2019-07-30 15:26:27 +0100

    restructuring

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "helloworld.h"
 
 using namespace std;
 
 int main()
 {
+    printTest();
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!", "and the C++ extension!"};
     for (const string& word : msg)
     {
---
+++ helloworld/helloworld.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void printTest() 
+{
+    std::cout << std::endl << "print from another file";
+}
